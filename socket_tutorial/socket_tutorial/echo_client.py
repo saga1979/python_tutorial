0930d2f086c2b66e6ed46f0fbf50a0d237106a6a
saga1979 <saga28@yeah.net>
2020-03-15 09:46:56 +0800

    vscode 修改测试

--- socket_tutorial/socket_tutorial/echo_client.py
+++ socket_tutorial/socket_tutorial/echo_client.py
@@ -5,8 +5,8 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
 client.connect(("localhost", 3333))
 
 while True:
-    client.send(b"hello")
-    
+    client.send(b"hello python")
+
     msg = client.recv(1024)
     print(msg.decode())
     time.sleep(1)
